409f669f9b33ff57bca39a50a0ffea1fa3907a4e
vovanz <vovanzagorodsky@gmail.com>
2018-04-24 01:15:17 +0500

    mnemonic hash

--- mnemonics/mnemonic_hash.py
+++ mnemonics/mnemonic_hash.py
@@ -1,2 +1,15 @@
-class MnemonicHash:
-    pass
+from random import Random
+
+from mnemonics.abstract.joinable import Joinable
+from mnemonics.wordlist import default_wordlist
+
+
+class MnemonicHash(Joinable):
+    wordlist = default_wordlist
+
+    def __init__(self, seed, length=4):
+        self._random = Random(seed)
+        self._mnemonic_list = [self._random.choice(self.wordlist) for _ in range(length)]
+
+    def __iter__(self):
+        return iter(self._mnemonic_list)
